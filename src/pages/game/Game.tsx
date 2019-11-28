c00f25ba17f76995a97b387920793220e47ea83c
Rémi Mafat <remimafat@gmail.com>
2019-11-28 10:53:49 +0100

    Add eslint

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import SoundWaves from "../../components/soundWaves/SoundWaves";
-import {useParams} from "react-router";
+import { useParams } from 'react-router-dom';
+import SoundWaves from '../../components/soundWaves/SoundWaves';
 
 const Game: React.FC = () => {
-    // Room id
-    let { id } = useParams();
+  // Room id
+  let {id} = useParams();
 
-    return (
-        <div className="container row">
-            <div className="col m3">
-                history
-            </div>
+  return (
+    <div className="container row">
+      <div className="col m3">
+        history
+      </div>
 
-            <div className="col m6">
-                <div>Dance</div>
+      <div className="col m6">
+        <div>Dance</div>
 
-                <div>
-                    <SoundWaves enable={true} />
-                </div>
-            </div>
-
-            <div className="col m3">
-                classement
-            </div>
+        <div>
+          <SoundWaves enable={true}/>
         </div>
-    )
+      </div>
+
+      <div className="col m3">
+        classement
+      </div>
+    </div>
+  )
 };
 
 export default Game;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,71 +1,72 @@
-import React, {useEffect, useState} from 'react';
-import './Home.scss'
-import {fetchGenres} from "../../utils/requests";
-import {Genre} from "../../models/Genre";
-import {useHistory} from "react-router";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Modal from 'react-modal';
-import {ROUTES} from "../../config/routes";
+import { fetchGenres } from '../../utils/requests';
+import './Home.scss';
+import { ROUTES } from '../../config/routes';
+import { Genre } from '../../models/Genre';
 
 const Home: React.FC = () => {
-    const history = useHistory();
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [selectedGenreId, setSelectedGenreId] = useState(0);
+  const history = useHistory();
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectedGenreId, setSelectedGenreId] = useState(0);
 
-    const openModal = (genreId: number) => {
-        setSelectedGenreId(genreId);
-        setIsOpen(true);
-    };
-    const closeModal = () => {
-        setIsOpen(false);
-    };
+  const openModal = (genreId: number) => {
+    setSelectedGenreId(genreId);
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
-    const getGenres = async () => {
-        const genres: Genre[] = await fetchGenres();
-        setGenres(genres);
-    };
+  const getGenres = async () => {
+    const genres: Genre[] = await fetchGenres();
+    setGenres(genres);
+  };
 
-    useEffect(() => {
-        getGenres()
-    }, []);
+  useEffect(() => {
+    getGenres();
+  }, []);
 
-    return (
-        <div>
-            <p>Choisissez un thème parmi ceux proposés</p>
-            <div className="genres-container">
-                {genres ? genres.map(genre => (
-                    <div className="genre-item" key={genre.id} onClick={() => openModal(genre.id)}>
-                        <img src={genre.picture_url} alt="" height={150} width={250}/>
-                        <p>{genre.name}</p>
-                    </div>
-                )) : <p>No Genres found</p>}
-            </div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}>
-                <div className="modal-buttons">
-                    <div
-                        className="btn btn-large btn-public"
-                        onClick={() => history.push(`${ROUTES.GAME}/${selectedGenreId}`)}
-                    >
-                        Rejoindre une partie publique
-                    </div>
-                    <div className="btn btn-large blue">Créer une partie privée</div>
-                </div>
-            </Modal>
+  return (
+    <div>
+      <p>Choisissez un thème parmi ceux proposés</p>
+      <div className="genres-container">
+        {genres ? genres.map((genre) => (
+          <div className="genre-item" key={genre.id} onClick={() => openModal(genre.id)}>
+            <img src={genre.picture_url} alt="" height={150} width={250} />
+            <p>{genre.name}</p>
+          </div>
+        )) : <p>No Genres found</p>}
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div className="modal-buttons">
+          <div
+            className="btn btn-large btn-public"
+            onClick={() => history.push(`${ROUTES.GAME}/${selectedGenreId}`)}
+          >
+            Rejoindre une partie publique
+          </div>
+          <div className="btn btn-large blue">Créer une partie privée</div>
         </div>
-    )
+      </Modal>
+    </div>
+  );
 };
 
 export default Home;
 
 const customStyles = {
-    content: {
-        borderRadius: '15px',
-        top: '35%',
-        left: '30%',
-        right: '30%',
-        bottom: '48%',
-    }
+  content: {
+    borderRadius: '15px',
+    top: '35%',
+    left: '30%',
+    right: '30%',
+    bottom: '48%',
+  },
 };
